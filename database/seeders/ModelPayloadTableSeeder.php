75617ca39e4ac7b8c692a208a8de081ae30d2b1e
elynrose29 <elynrose29@gmail.com>
2025-03-11 15:26:24 -0400

    Update database seeder

--- database/seeders/ModelPayloadTableSeeder.php
+++ database/seeders/ModelPayloadTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class ModelPayloadSeeder extends Seeder
+class ModelPayloadTableSeeder extends Seeder
 {
     public function run()
     {
